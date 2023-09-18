5b2a9c6c946378d45a1cadf043b02f2b3d2cc559
Manikandan Sundararajan <mani@hey.com>
2023-09-18 18:09:15 -0400

    read a file and print the hexdump

--- fview/src/main.rs
+++ fview/src/main.rs
@@ -1,24 +1,28 @@
+use std::env;
+use std::fs::File;
 use std::io::prelude::*;
 
 const BYTES_PER_LINE: usize = 16;
-const INPUT: &'static [u8] = br#"
+
 fn main() {
-    println!("Hello, world!");
-}"#;
+    let arg1 = env::args().nth(1);
 
-fn main() -> std::io::Result<()> {
-    let mut buffer: Vec<u8> = vec![];
-    INPUT.read_to_end(&mut buffer)?;
+    let fname = arg1.expect("usage: fview FILENAME");
 
-    let mut position_in_input = 0;
-    for line in buffer.chunks(BYTES_PER_LINE) {
-        print!("[0x{:08x}] ", position_in_input);
-        for byte in line {
-            print!("{:02x} ", byte);
+    let mut f = File::open(&fname).expect("Unable to Open the File");
+    let mut pos = 0;
+    let mut buffer = [0; BYTES_PER_LINE];
+
+    while let Ok(_) = f.read_exact(&mut buffer) {
+        print!("[0x{:08x}] ", pos);
+        for byte in &buffer {
+            match *byte {
+                0x00 => print!(".  "),
+                0xff => print!("## "),
+                _ => print!("{:02x} ", byte),
+            }
         }
         println!();
-        position_in_input += BYTES_PER_LINE;
+        pos += BYTES_PER_LINE;
     }
-
-    Ok(())
 }
